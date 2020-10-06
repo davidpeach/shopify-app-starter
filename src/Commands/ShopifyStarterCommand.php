8b5673836b4a4e3bbb7069a8ab002c4020cc3df8
David Peach <mail@davidpeach.co.uk>
2020-10-06 21:11:45 +0100

    Tidy current commands and structure.

--- src/Commands/ShopifyStarterCommand.php
+++ src/Commands/ShopifyStarterCommand.php
@@ -14,6 +14,25 @@ class ShopifyStarterCommand extends Command
      */
     protected $signature = 'davidpeach:shopify-app-setup';
 
+    private $projectName;
+
+    private $composerPackages = [
+        'esc/shopify:v2.x-dev',
+    ];
+
+    private $npmDevPackages = [
+        '@eastsideco/polaris-vue@^0.1.19',
+        'evee',
+        'vue',
+        'vue-router',
+        'vue-template-compiler',
+        'vuex',
+    ];
+
+    const ANSWER_YES = 'Yes';
+
+    const ANSWER_NO = 'Nope';
+
     /**
      * Create a new command instance.
      *
@@ -31,31 +50,53 @@ class ShopifyStarterCommand extends Command
      */
     public function handle()
     {
-        $projectName = $this->ask('What is your project name?');
+        $this->projectName = $this->ask('What is your project name?');
 
-        $installNpmPackages = $this->choice(
-            'Install NPM Packages?',
-            ['Yes, please', 'Nah, fam.'],
+        $this->installComposerPackages = $this->choice(
+            'Install Composer Packages?',
+            [self::ANSWER_YES, self::ANSWER_NO],
             0
         );
 
-        $npmDevPackages = collect([
-            '@eastsideco/polaris-vue@^0.1.19',
-            'evee',
-            'vue',
-            'vue-router',
-            'vue-template-compiler',
-            'vuex',
-        ]);
+        $this->installNpmPackages = $this->choice(
+            'Install NPM Packages?',
+            [self::ANSWER_YES, self::ANSWER_NO],
+            0
+        );
 
-        if ($installNpmPackages) {
-            $npmDevPackages->each(function ($package) {
-                $process = new Process(
-                    ['npm', 'install', '--save-dev', $package]
-                );
-                $process->run();
-                $this->info($process->getOutput());
-            });
-        }
+        $this->performSetupSteps();
+    }
+
+    private function performSetupSteps()
+    {
+        $this->attemptComposerPackageInstalls();
+        $this->attemptNpmPackageInstalls();
+    }
+
+    private function attemptComposerPackageInstalls()
+    {
+        if ($this->installComposerPackages === self::ANSWER_NO) return;
+
+        collect($this->composerPackages)->each(function ($package) {
+            $process = new Process(
+                ['composer', 'require', $package]
+            );
+            $process->run();
+
+            $this->info($process->getOutput());
+        });
+    }
+
+    private function attemptNpmPackageInstalls()
+    {
+        if ($this->installNpmPackages === self::ANSWER_NO) return;
+
+        collect($this->npmDevPackages)->each(function ($package) {
+            $process = new Process(
+                ['npm', 'install', '--save-dev', $package]
+            );
+            $process->run();
+            $this->info($process->getOutput());
+        });
     }
 }
